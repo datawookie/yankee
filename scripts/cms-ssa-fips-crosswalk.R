4b176ad6c6491d693b598c1e77cd486e485fefed
Andrew Collier <andrew@exegetic.biz>
2021-02-04 05:41:04 +0200

    Add in missing SSA state codes

--- scripts/cms-ssa-fips-crosswalk.R
+++ scripts/cms-ssa-fips-crosswalk.R
@@ -2,6 +2,10 @@ library(readr)
 library(janitor)
 library(dplyr)
 
+# NOTES:
+#
+# 1. Run the fips-create.R script first.
+
 # Get full set of SSA codes used in CMS data.
 #
 # These are from the CMS Extracts "claimsk" files.
@@ -26,6 +30,12 @@ ssa_fips <- read_csv(
     fips = fips_county_code
   )
 
+ssa_state <- read_delim(
+  here::here("data-raw", "ssa-state.csv"),
+  delim = ";",
+  col_types = "ic"
+)
+
 ssa_county <- cms_ssa %>%
   left_join(ssa_fips, by = "ssa") %>%
   mutate(
@@ -41,13 +51,18 @@ ssa_county <- ssa_county %>%
     fips,
     matches("^fips_"),
     county_name
-  )
+  ) %>%
+  unique()
 
-ssa_state <- ssa_county %>%
-  select(ssa_state, fips_state) %>%
-  na.omit() %>%
-  unique() %>%
-  inner_join(fips_state, by = "fips_state") %>%
+ssa_state <- ssa_state %>%
+  left_join(
+    ssa_county %>%
+      select(ssa_state, fips_state) %>%
+      na.omit() %>%
+      unique(),
+    by = "ssa_state"
+  ) %>%
+  left_join(fips_state %>% select(-state_name), by = "fips_state") %>%
   select(ssa_state, fips_state, state_name, state_abbr)
 
 usethis::use_data(ssa_state, overwrite = TRUE)
